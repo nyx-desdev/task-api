edeabd814b49a7bad9c123b96c297e86d3f9c0d1
nyx-desdev <nyx.desdev@gmail.com>
2021-08-14 21:59:07 +0530

    change port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.listen(3090, () => {
+app.listen(port, () => {
 	console.log(`Server is running on port 3090`);
 });
